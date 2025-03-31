aeaaa9354737f654e70819ccdd2c1f09ca349482
jieun4510@sju.ac.kr <jieun4510@sju.ac.kr>
2025-03-31 23:46:54 +0900

    initial commit

---
+++ src/main/java/com/example/dogcument/DogcumentApplication.java
@@ -0,0 +1,13 @@
+package com.example.dogcument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogcumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogcumentApplication.class, args);
+	}
+
+}
